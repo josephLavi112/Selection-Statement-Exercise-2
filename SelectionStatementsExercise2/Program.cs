5f427b51ee1851ad0aaa1258b0e3870fd68b8799
Joseph LaVia <josephdlavia@gmail.com>
2023-05-15 14:52:10 -0500

    Favorite School Subject

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Collections;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "Science":
+                    Console.WriteLine("I love dissecting frogs");
+                    break;
+
+                case "Math":
+                    Console.WriteLine("Math makes my head hurt");
+                    break;
+
+                case "History":
+                    Console.WriteLine("History repeats itself");
+                    break;
+
+                case "Culianry":
+                    Console.WriteLine("Dont burn the kitchen down");
+                    break;
+
+                case "Economics":
+                    Console.WriteLine("Lets get rich");
+                    break;
+            }
+
+
+
         }
     }
 }
